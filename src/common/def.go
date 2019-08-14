bb1eb2c5fc09490ba3cca022d886156c25ce9f03
TATangZY <tangdachan@qq.com>
2019-08-14 14:20:09 +0800

    update

--- src/common/def.go
+++ src/common/def.go
@@ -1,5 +1,10 @@
 package common
 
+//Register 注册专用参数
 type Register struct {
 	ID string
+}
+
+//Args 通用参数
+type Args struct {
 }
--- src/main.go
+++ src/main.go
@@ -7,18 +7,22 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"sync"
 	"time"
 
 	"github.com/cenkalti/rpc2"
 	_ "github.com/go-sql-driver/mysql"
 )
 
+//ClientInfo 客户端的信息
 type ClientInfo struct {
-	Client *rpc2.Client
+	Client  *rpc2.Client
+	IsReady bool
 }
 
 var clients, taskRunners map[string]*ClientInfo
 var db *sql.DB
+var mutex sync.Mutex
 
 func main() {
 	flag.Parse()
@@ -26,53 +30,71 @@ func main() {
 		panic("usage: host port") //检查参数是否正确
 	}
 
+	//初始化
 	clients = make(map[string]*ClientInfo)
 	taskRunners = make(map[string]*ClientInfo)
+	srv := rpc2.NewServer()
 
+	//连接数据库
 	var err error
 	db, err = sql.Open("mysql", "username:passwd@tcp(IP:port)/dbname/charset=utf8") //连接到数据库
 	ec.CheckError(err, "Open database: ")
 
-	srv := rpc2.NewServer()
-
+	//连接和断开连接时触发的事件
 	srv.OnConnect(func(client *rpc2.Client) {
 		fmt.Println("connect:")
 	})
 	srv.OnDisconnect(disconnect)
 
+	//注册函数，使这些函数可以被远程调用
 	srv.Handle("clientRegister", clientRegister)
 	srv.Handle("taskRunnerRegister", taskRunnerRegister)
-	srv.Handle("clientGetReady", clientGetReady)
+	srv.Handle("clientGetReady", clientGetReadyandAssignTask)
+	srv.Handle("taskRunnerGetReady", taskRunnerGetReady)
 
+	//初始化及开始监听
 	hostAndPort := fmt.Sprintf("%s:%s", flag.Arg(0), flag.Arg(1)) //ip port
 	serverAddr, err := net.ResolveTCPAddr("tcp", hostAndPort)
 	ec.CheckError(err, "Resolving address:port failed: '"+hostAndPort+"'")
+	lis, err := net.ListenTCP("tcp", serverAddr)
+	ec.CheckError(err, "Listen TCP: ")
 	go func() {
-		lis, err := net.ListenTCP("tcp", serverAddr)
-		ec.CheckError(err, "Listen TCP: ")
 		srv.Accept(lis)
+		defer lis.Close()
 	}()
 
 	time.Sleep(1 * time.Second)
 
 	for { //TODO
+		fmt.Println("Enter 'q' to quit ...")
+		var input string
+		fmt.Scanln(&input)
 
+		if input == "q" {
+			break
+		}
 	}
 }
 
-func clientRegister(client *rpc2.Client, args *common.Register, res *string) error { //TODO
+func clientRegister(client *rpc2.Client, args *common.Register, res *string) error {
 	clients[args.ID] = &ClientInfo{Client: client}
 	//sql
 	stmt, err := db.Prepare(`INSERT INTO devices VALUES(?, ?, ?)`)
 	ec.CheckError(err, "Prepare sql:")
-	_, err = stmt.Exec(args.ID, nil, "idle")
+	_, err = stmt.Exec(args.ID, nil, "idle") //TODO
 
 	fmt.Println("Add a client: ")
 	*res = "ok"
 	return nil
 }
 
-func taskRunnerRegister(client *rpc2.Client, res *string) error { //TODO
+func taskRunnerRegister(client *rpc2.Client, args *common.Register, res *string) error {
+	taskRunners[args.ID] = &ClientInfo{Client: client}
+
+	stmt, err := db.Prepare(`INSERT INTO taskrunners VALUES(?, ?, ?)`)
+	ec.CheckError(err, "Prepare sql: ")
+	_, err = stmt.Exec(args.ID, "idle") //TODO
+
 	fmt.Println("Add a task runner: ")
 	*res = "ok"
 	return nil
@@ -88,11 +110,48 @@ func disconnect(client *rpc2.Client) {
 	}
 }
 
-func clientGetReady(client *rpc2.Client, res *string) error { //TODO sql
+func clientGetReadyandAssignTask(client *rpc2.Client, res *string) error { //TODO sql
 	stmt, err := db.Prepare(`UPDATE ...`)
 	ec.CheckError(err, "Prepare sql: ")
 	_, err = stmt.Exec("ready")
-	ec.CheckError(err, "client get ready: ")
+	ec.CheckError(err, "Client get ready: ")
 
+	for _, v := range clients {
+		if v.Client == client {
+			v.IsReady = true
+			break
+		}
+	}
+
+	var reply string
+	client.Call("sendTask", &common.Args{}, &reply) //TODO 添加参数内容
+	fmt.Println("Send task: ", reply)
+
+	mutex.Lock()
+	for _, v := range taskRunners {
+		if v.IsReady == true {
+			v.IsReady = false
+			v.Client.Call("getTask", &common.Args{}, &reply) //TODO 添加参数内容
+			fmt.Println("Get task: ", reply)
+			break
+		}
+	}
+	mutex.Unlock()
+
+	return nil
+}
+
+func taskRunnerGetReady(client *rpc2.Client, args *common.Args, res *string) error {
+	stmt, err := db.Prepare(`UPDATE ...`)
+	ec.CheckError(err, "Prepare sql: ")
+	_, err = stmt.Exec("ready")
+	ec.CheckError(err, "Task runner get ready: ")
+
+	for _, v := range taskRunners {
+		if v.Client == client {
+			v.IsReady = true
+			break
+		}
+	}
 	return nil
 }
